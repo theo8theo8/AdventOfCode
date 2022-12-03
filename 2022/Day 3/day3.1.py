1456ec45828b0e1aac322d230f6fd8a12932bd43
Theo Meier Ström <meier.strom.theo@gmail.com>
2022-12-03 12:17:45 +0100

    small fix

--- 2022/Day 3/day3.1.py
+++ 2022/Day 3/day3.1.py
@@ -3,8 +3,7 @@ with open('day3.txt') as f:
     input = f.read().splitlines()
     for row in input:
         comp_1, comp_2 = row[:len(row)//2], row[len(row)//2:]
-        intersect = set(comp_1).intersection(set(comp_2))
-        char = intersect.pop()
+        char = set(comp_1).intersection(set(comp_2)).pop()
         if char.islower():
             sum += ord(char)-96
         else:
--- 2022/Day 3/day3.2.py
+++ 2022/Day 3/day3.2.py
@@ -2,8 +2,7 @@ sum = 0
 with open('day3.txt') as f:
     input = f.read().splitlines()
     for i in range(0,len(input),3):
-        intersect = set(input[i]).intersection(set(input[i+1])).intersection(set(input[i+2]))
-        char = intersect.pop()
+        char = set(input[i]).intersection(set(input[i+1])).intersection(set(input[i+2])).pop()
         if char.islower():
             sum += ord(char)-96
         else:
